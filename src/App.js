b60e20bbaa4bec59a26df983a719a4d51c929af0
rony646 <rony646@users.noreply.github.com>
2021-04-25 17:45:24 -0300

    adding test to the button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import { useState } from 'react';
 import './App.css';
-import Counter from './Counter/Counter';
 
 function App() {
+
+  const [ buttonColor, setButtonColor ] = useState('red');
+
+  const newButtonColor = buttonColor === 'red' ? 'blue' : 'red';
+
   return (
     <div className="App">
-        <Counter />
+        <button 
+          style={{backgroundColor: buttonColor}} 
+          onClick={() => setButtonColor(newButtonColor)}
+        >
+          Change to {newButtonColor}
+        </button>
     </div>
   );
 }
